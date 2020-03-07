ca93424804aa3866e07d8d89870de723df8402a6
relegated <35969849+relegated@users.noreply.github.com>
2020-03-07 09:18:17 -0700

    exported functions

--- getPostRate.js
+++ getPostRate.js
@@ -1,77 +1,93 @@
 module.exports = {
-    getRate : function(parcelType, weight) {
+    getPostRate: function (parcelType, weight) {
         switch (parcelType) {
             case 'letterStamped':
-              if (weight <= 1)
-                return 0.55;
-              if (weight <= 2)
-                return 0.7;
-              if (weight <= 3)
-                return 0.85;
-              return 1.00;
-        
+                if (weight <= 1)
+                    return 0.55;
+                if (weight <= 2)
+                    return 0.7;
+                if (weight <= 3)
+                    return 0.85;
+                return 1.00;
+
             case 'letterMetered':
-              if (weight <= 1)
-                return 0.5;
-              if (weight <= 2)
-                return 0.65;
-              if (weight <= 3)
-                return 0.8;
-              return 0.95;
-        
+                if (weight <= 1)
+                    return 0.5;
+                if (weight <= 2)
+                    return 0.65;
+                if (weight <= 3)
+                    return 0.8;
+                return 0.95;
+
             case 'largeEnv':
-              if (weight <= 1)
-                return 1.0;
-              if (weight <= 2)
-                return 1.2;
-              if (weight <= 3)
-                return 1.4;
-              if (weight <= 4)
-                return 1.6;
-              if (weight <= 5)
-                return 1.8;
-              if (weight <= 6)
-                return 2.0;
-              if (weight <= 7)
-                return 2.2;
-              if (weight <= 8)
-                return 2.4;
-              if (weight <= 9)
-                return 2.6;
-              if (weight <= 10)
-                return 2.8;
-              if (weight <= 11)
-                return 3.0;
-              if (weight <= 12)
-                return 3.2;
-              return 3.4;
-        
+                if (weight <= 1)
+                    return 1.0;
+                if (weight <= 2)
+                    return 1.2;
+                if (weight <= 3)
+                    return 1.4;
+                if (weight <= 4)
+                    return 1.6;
+                if (weight <= 5)
+                    return 1.8;
+                if (weight <= 6)
+                    return 2.0;
+                if (weight <= 7)
+                    return 2.2;
+                if (weight <= 8)
+                    return 2.4;
+                if (weight <= 9)
+                    return 2.6;
+                if (weight <= 10)
+                    return 2.8;
+                if (weight <= 11)
+                    return 3.0;
+                if (weight <= 12)
+                    return 3.2;
+                return 3.4;
+
             case 'firstClass':
-              if (weight <= 1)
-                return 3.8;
-              if (weight <= 2)
-                return 3.8;
-              if (weight <= 3)
-                return 3.8;
-              if (weight <= 4)
-                return 3.8;
-              if (weight <= 5)
-                return 4.6;
-              if (weight <= 6)
-                return 4.6;
-              if (weight <= 7)
-                return 4.6;
-              if (weight <= 8)
-                return 4.6;
-              if (weight <= 9)
-                return 5.3;
-              if (weight <= 10)
-                return 5.3;
-              if (weight <= 11)
-                return 5.3;
-              if (weight <= 12)
-                return 5.3;
-              return 5.9;
-          }
+                if (weight <= 1)
+                    return 3.8;
+                if (weight <= 2)
+                    return 3.8;
+                if (weight <= 3)
+                    return 3.8;
+                if (weight <= 4)
+                    return 3.8;
+                if (weight <= 5)
+                    return 4.6;
+                if (weight <= 6)
+                    return 4.6;
+                if (weight <= 7)
+                    return 4.6;
+                if (weight <= 8)
+                    return 4.6;
+                if (weight <= 9)
+                    return 5.3;
+                if (weight <= 10)
+                    return 5.3;
+                if (weight <= 11)
+                    return 5.3;
+                if (weight <= 12)
+                    return 5.3;
+                return 5.9;
+        }
+    },
+    getParcelTypeDescriptionString: function (name) {
+        switch (name) {
+            case "letterStamped":
+
+                return "Letters (Stamped)";
+            case "letterMetered":
+                return "Letters (Metered)";
+            case "largeEnv":
+                return "Large Envelopes (Flats)";
+            case "firstClass":
+                return "First-Class Package";
+
+            default:
+                break;
+        }
     }
 };
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const rateCalc = require('getPostRate');
+const rateCalc = require('./getPostRate');
 const PORT = process.env.PORT || 5000;
 
 express()
@@ -8,7 +8,6 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/prove9', (req, res) => res.render('staticpages/prove9'))
   .get('/computePostage', computePostage)
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
@@ -16,9 +15,11 @@ function computePostage(req, res) {
   const parcelType = req.query.postType;
   const weight = Number(req.query.weight);
 
-  let rate = getRate(parcelType, weight);
+  let rate = rateCalc.getPostRate(parcelType, weight);
 
-  const params = {parcelType: parcelType, weight: weight, rate: rate};
+  let description = rateCalc.getParcelTypeDescriptionString(parcelType);
+
+  const params = {parcelType: description, weight: weight, rate: rate};
 
   res.render('pages/prove9', params);
 }
