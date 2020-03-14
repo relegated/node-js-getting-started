9a4570197b9621f3a1c988e84d64e84106f0fa8b
relegated <35969849+relegated@users.noreply.github.com>
2020-03-14 12:55:53 -0600

    insert user

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const passwordHash = requer('password-hash');
 const rateCalc = require('./getPostRate');
 const PORT = process.env.PORT || 5000;
 
@@ -10,7 +11,7 @@ const { Pool } = require("pg");
 const pool = new Pool({ connectionString: connectionString });
 
 express()
-  .use(bodyParser.urlencoded({ extended:false }))
+  .use(bodyParser.urlencoded({ extended: false }))
   .use(bodyParser.json())
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -18,6 +19,7 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/computePostage', computePostage)
   .get('/isUserNameAvailable', checkUsernameAvailable)
+  .post('/signUp', addUser)
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 function computePostage(req, res) {
@@ -28,7 +30,7 @@ function computePostage(req, res) {
 
   let description = rateCalc.getParcelTypeDescriptionString(parcelType);
 
-  const params = {parcelType: description, weight: weight, rate: rate};
+  const params = { parcelType: description, weight: weight, rate: rate };
 
   res.render('pages/prove9', params);
 }
@@ -46,12 +48,32 @@ function checkUsernameAvailable(req, response) {
       console.log(`Error in query: ${err}`);
     }
     let usercount = res.rows[0];
-    if (Number(usercount.count) > 0){
+    if (Number(usercount.count) > 0) {
       response.status(409).send(JSON.stringify(usercount));
     }
     else {
       response.status(200).send(JSON.stringify(usercount));
     }
-    
+
+  });
+}
+
+function addUser(req, response) {
+  const username = req.query.username;
+  const pass = req.query.password;
+  let hashedPass = passwordHash.generate(pass);
+
+  const sql = "INSERT INTO user_table (username, password_hash, user_level) VALUES ($1, $2, $3::int)";
+  const params = [username, hashedPass, 1];
+  response.type('application/json');
+
+  pool.query(sql, params, (err, res) => {
+    if (err) {
+      console.log(`Error in query: ${err}`);
+      response.status(500).send({ error: "Database error" });
+    }
+    else {
+      response.status(200).send({ result: "User added" });
+    }
   });
 }
